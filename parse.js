2d679eaeaddafeb0557e8f90cfb8327615036b75
minexew <minexew@gmail.com>
2018-01-08 00:55:57 +0100

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+const DiagnosticsPrinter = require('./utility/DiagnosticsPrinter');
 const Dmparser = require('./dmparser');
 const SimpleFileAccessor = require('./utility/SimpleFileAccessor');
 
 async function asyncMain() {
-	const parser = new Dmparser(new SimpleFileAccessor());
+	const sfa = new SimpleFileAccessor();
+	const parser = new Dmparser(sfa, new DiagnosticsPrinter(sfa));
 
 	const argv = process.argv;
 	//console.log(argv);
